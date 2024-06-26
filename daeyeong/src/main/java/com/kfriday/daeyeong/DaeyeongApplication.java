9367aa4133359f966166f1f9f92ea857915f9654
kdozlo <eodud5730@naver.com>
2024-06-26 14:47:15 +0900

    feat: 프로젝트 초기 설정

---
+++ daeyeong/src/main/java/com/kfriday/daeyeong/DaeyeongApplication.java
@@ -0,0 +1,13 @@
+package com.kfriday.daeyeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaeyeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaeyeongApplication.class, args);
+	}
+
+}
---
+++ daeyeong/src/test/java/com/kfriday/daeyeong/DaeyeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfriday.daeyeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaeyeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
